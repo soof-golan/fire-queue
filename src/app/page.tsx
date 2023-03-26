552b3094a8bf5dcb83d07340a4abc33fdebd2c01
Soof Golan <soofgolan@gmail.com>
2023-03-26 19:27:56 +0300

    feat: first working query to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
 import styles from './page.module.css'
 import SignInButton from "@/app/components/SignInButton";
+import {getServerSession} from "next-auth";
+import prisma from "@/prismaClient";
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user || !session.user.email) {
+    return (
+      <main className={styles.main}>
+        <SignInButton/>
+      </main>
+    )
+  }
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    },
+    include: {
+      ownedEvents: true,
+    }
+  })
+
+  if (!user) {
+    return (
+      <main className={styles.main}>
+        <SignInButton/>
+      </main>
+    )
+  }
+
   return (
     <main className={styles.main}>
-      <SignInButton/>
+      <div>
+        You are signed in as {user?.email}
+      </div>
+      <div>
+        You have {user?.ownedEvents?.length} events.
+      </div>
     </main>
   )
 }
