7751a186220436da4675ae88aa0f5d4b3a5ee1da
Soof Golan <soofgolan@gmail.com>
2023-03-26 19:22:10 +0300

    auth: oops, forgot to add next auth api route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, {AuthOptions} from "next-auth"
 import EmailProvider from "next-auth/providers/email"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import {PrismaAdapter} from "@next-auth/prisma-adapter"
+import {PrismaClient} from "@prisma/client"
 
 const prisma = new PrismaClient()
 
