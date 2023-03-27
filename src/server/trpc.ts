1d701876d656ea45a6fb817390e32a4d515dd239
Soof Golan <soofgolan@gmail.com>
2023-03-27 11:12:47 +0300

    auth: add session context to tRPC

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,10 +1,21 @@
 import { initTRPC } from '@trpc/server';
+import * as trpcNext from "@trpc/server/adapters/next";
+import getBetterServerSession from "@/auth/getBetterServerSession";
+
+// export API handler
+// @see https://trpc.io/docs/api-handler
+export function createContext({req, res}: trpcNext.CreateNextContextOptions) {
+  const session = getBetterServerSession(req, res);
+  return {
+    session,
+  }
+}
 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create();
+const t = initTRPC.context<typeof createContext>().create();
 
 // Base router and procedure helpers
 export const router = t.router;
