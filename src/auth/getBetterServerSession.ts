a9e4346886fe1bdd0be80375a594336fa8bab6d8
Soof Golan <soofgolan@gmail.com>
2023-03-27 11:05:18 +0300

    feat: getBetterServerSession supports inferring sessions from requests

--- src/auth/getBetterServerSession.ts
+++ src/auth/getBetterServerSession.ts
@@ -1,6 +1,6 @@
-import "server-only";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {GetServerSidePropsContext} from "next";
 
 type SessionWithUser = Awaited<ReturnType<typeof getServerSession>> & {
   user: {
@@ -10,7 +10,9 @@ type SessionWithUser = Awaited<ReturnType<typeof getServerSession>> & {
 
 // The user id was added in the next-auth session callback, we're just making typescript happy here
 // See authOptions
-export default async function getBetterServerSession(): Promise<SessionWithUser> {
-
-  return await getServerSession(authOptions) as SessionWithUser
+export default async function getBetterServerSession(req?: GetServerSidePropsContext["req"], res?: GetServerSidePropsContext["res"]): Promise<SessionWithUser> {
+  if (!req || !res) {
+    return await getServerSession(authOptions) as SessionWithUser
+  }
+  return await getServerSession(req, res, authOptions) as SessionWithUser
 }
